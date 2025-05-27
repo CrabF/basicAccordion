0d6faed01dd95ab01746b58d999b2fd4b966bae8
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2025-05-27 22:24:28 +0300

    fix: remove tags details and summary, add div and animation, rework js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const details = document.querySelectorAll("details");
+const faq = document.querySelector(".faq");
+const points = document.querySelectorAll(".details");
 
-details.forEach((detail) => {
-  const summary = detail.querySelector("summary");
-  const img = summary.querySelector("img");
+faq.addEventListener("click", (e) => {
+  const li = e.target.closest(".details");
+  if (e.target.closest(".summary")) {
+    closeAnotherPoints(li);
+    const summaryNode = e.target.closest(".summary");
+    const text = summaryNode.nextElementSibling;
+    text.classList.toggle("hide");
 
-  img.src = detail.open
-    ? "./assets/images/icon-minus.svg"
-    : "./assets/images/icon-plus.svg";
-  img.alt = detail.open ? "minus" : "plus";
+    const img = summaryNode.querySelector("img");
+    img.classList.toggle("rotated");
+    img.src = text.classList.contains("hide")
+      ? "./assets/images/icon-plus.svg"
+      : "./assets/images/icon-minus.svg";
+    img.alt = text.classList.contains("hide") ? "plus" : "minus";
+  }
+});
 
-  summary.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    details.forEach((d) => {
-      if (d !== detail) {
-        d.removeAttribute("open");
-        const otherImg = d.querySelector("img");
-        otherImg.src = "./assets/images/icon-plus.svg";
-        otherImg.alt = "plus";
-      }
-    });
-
-    if (detail.open) {
-      detail.removeAttribute("open");
+function closeAnotherPoints(point) {
+  points.forEach((item) => {
+    if (item != point) {
+      const text = item.querySelector(".text");
+      const img = item.querySelector("img");
+      img.classList.remove("rotated");
       img.src = "./assets/images/icon-plus.svg";
       img.alt = "plus";
-    } else {
-      detail.setAttribute("open", "");
-      img.src = "./assets/images/icon-minus.svg";
-      img.alt = "minus";
+      text.classList.add("hide");
     }
   });
-});
+}
